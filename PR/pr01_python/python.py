53823e5645852a8e3f93cd8aacb152d5f1b900b6
elgedr <elgedr@ttu.ee>
2020-09-01 15:28:41 +0300

    Test2

--- PR/pr01_python/python.py
+++ PR/pr01_python/python.py
@@ -1,6 +1,6 @@
 name = input('what is your name? ')
 age = int(input(f' Hello, {name}. What year were you born in? '))
 
-if age < 2008 :
+if age < 2008:
     print(f'You were {2008 - age} years old when Python 3.0 was released')
 else: print(f'Python 3 was {age - 2008 } years old when you were born.')
