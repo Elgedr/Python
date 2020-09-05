2fd3427ed9a49c3af2d614a466e17658b83c563d
elgedr <elgedr@ttu.ee>
2020-09-05 20:22:45 +0300

    Ulesanne

--- EX/ex01_cashier/cashier.py
+++ EX/ex01_cashier/cashier.py
@@ -1,11 +1,10 @@
+"""Cashier."""
 coins_list = [50, 20, 10, 5, 1]
 summa = int(input('Enter a sum: '))
 coins = 0
 for i in coins_list:
-    print(f"Сейчас работаем с монетой: {i}")
     number1 = summa//i
-    print(number1)
     coins = coins + number1
-    print(coins)
     summa = summa - i * number1
-    print(summa)
+
+print(f"amount of coins needed: {summa}")
