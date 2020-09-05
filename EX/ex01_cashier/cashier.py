1fc7cbea751d66ac567b5c8ce9752960b2c80226
elgedr <elgedr@ttu.ee>
2020-09-05 21:44:33 +0300

    Ulesanne

--- EX/ex01_cashier/cashier.py
+++ EX/ex01_cashier/cashier.py
@@ -1,10 +1,9 @@
 """Cashier."""
 coins_list = [50, 20, 10, 5, 1]
-sentide_arv_ = int(input('Enter a sum: '))
+summa = int(input('Enter a sum: '))
 coins = 0
-for i in coins_list:
-    number1 = sentide_arv_ // i
+for i in range(len(coins_list)):
+    number1 = summa // (coins_list[i])
     coins = coins + number1
-    sentide_arv_ = sentide_arv_ - i * number1
-
-print(f"Amount of coins needed: {sentide_arv_}")
+    summa = summa - (coins_list[i] * number1)
+print(coins)
