d3422ebdd870bdb7066e74ef0fba78dca08db82b
elgedr <elgedr@ttu.ee>
2020-09-07 18:49:13 +0300

    Ulesanne

--- PR/pr02_primes/primes.py
+++ PR/pr02_primes/primes.py
@@ -11,8 +11,8 @@ def is_prime_number(x: int):
     for i in range(2, x):
         if x % i == 0:
             return False
-        else:
-            return True
+    else:
+        return True
 
 
 if __name__ == '__main__':
