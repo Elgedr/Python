e91d9658099ec77316c687c20ef53b6edbc7a660
elgedr <elgedr@ttu.ee>
2020-09-28 18:09:40 +0300

    ff

--- EX/ex04_hobbies/hobbies.py
+++ EX/ex04_hobbies/hobbies.py
@@ -54,20 +54,25 @@ def find_person_with_least_hobbies(file) -> list:
 
 def find_most_popular_hobby(file) -> list:
     """Find the most popular hobby."""
-    people_hobbies_dict = create_dictionary(file)
-    all_hobbies = list(people_hobbies_dict.values())
-    the_most_popular_hobby = []
-    hobby_amount = {}
-    for hobby in all_hobbies:
-        hobby_amount[hobby] = all_hobbies.count(hobby)
-    for hobby in hobby_amount:
-        if hobby_amount[hobby] == hobby_amount[max(hobby_amount, key=hobby_amount.get)]:
-            the_most_popular_hobby.append(hobby)
-    return the_most_popular_hobby
-
+    dictionary_from_names_and_hobbies = create_dictionary(file)  # словарь  в виде {"Mari": ["gym", "eating", "run"]}
+    dictionary_with_values = {}  # словарь где {ключ = кол-во этих хобби: значение = само хобби}
+    list_from_values = []
+    for value in dictionary_from_names_and_hobbies.values():  # рассматриваем сразу значения из словаря по отдельности
+        list_from_values = list_from_values + value  # добавляем все хобби в список
+    for hobby in set(list_from_values):  # set используем для того, чтобы все элементы были разные
+        dictionary_with_values.setdefault(list_from_values.count(hobby), []).append(hobby)  # добавляет {key = кол-во хобби : value = хобби }
+    return dictionary_with_values[max(dictionary_with_values)]  # возвращаем значение с самым наибольшем ключем
 
 def find_least_popular_hobby(file):
     """Find the least popular hobby."""
+    dictionary_from_names_and_hobbies = create_dictionary(file)
+    dictionary_with_values = {}
+    list_from_values = []
+    for value in dictionary_from_names_and_hobbies.values():
+        list_from_values = list_from_values + value
+    for hobby in set(list_from_values):
+        dictionary_with_values.setdefault(list_from_values.count(hobby), []).append(hobby)
+    return dictionary_with_values[min(dictionary_with_values)]
 
 
 def write_corrected_database(file, file_to_write):
@@ -79,4 +84,23 @@ def write_corrected_database(file, file_to_write):
         writer.writerow([name, hobbies])
 
 if __name__ == '__main__':
-    print(find_most_popular_hobby({"molli": ['gaming', 'sport', 'football'], "peter": ['gaming', 'sport']}))
+    dic = create_dictionary("hobbies_database.txt")
+    print(len(create_list_from_file("hobbies_database.txt")))  # -> 100
+    print("Check presence of hobbies for chosen person:")
+    print("shopping" in dic["Wendy"])  # -> True
+    print("fitness" in dic["Sophie"])  # -> False
+    print("gaming" in dic["Peter"])  # -> True
+    print("Check if hobbies - person relation is correct:")
+    print("Check if a person(people) with the biggest amount of hobbies is(are) correct:")
+    print(find_person_with_most_hobbies("hobbies_database.txt"))  # -> ['Jack']
+    print(len(dic["Jack"])) # ->  12
+    print(len(dic["Carmen"])) # -> 10
+    print("Check if a person(people) with the smallest amount of hobbies is(are) correct:")
+    print(find_person_with_least_hobbies("hobbies_database.txt"))  # -> ['Molly']
+    print(len(dic["Molly"])) # -> 5
+    print(len(dic["Sophie"])) # -> 7
+    print("Check if the most popular hobby(ies) is(are) correct")
+    print(find_most_popular_hobby("hobbies_database.txt"))  # -> ['gaming', 'sport', 'football']
+    print("Check if the least popular hobby(ies) is(are) correct")
+    print(find_least_popular_hobby("hobbies_database.txt"))  # -> ['tennis', 'dance', 'puzzles', 'flowers']
+    write_corrected_database("hobbies_database.txt", 'correct_hobbies_database.csv')
