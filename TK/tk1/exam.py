20ae755187e67ed2b4049f86c86cd005f26b3057
elgedr <elgedr@ttu.ee>
2020-09-29 14:12:52 +0300

    ff

--- TK/tk1/exam.py
+++ TK/tk1/exam.py
@@ -6,7 +6,10 @@ def workday_count(days):
     amount_of_days = days
     if amount_of_days <= 5:
         return amount_of_days
-    elif amount_of_days > 5:
+    elif amount_of_days == 6:
+        result = amount_of_days - 1
+        return result
+    elif amount_of_days ==7:
         result = amount_of_days - 2
         return result
 
