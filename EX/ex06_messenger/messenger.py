e4e8237e4455aa26b025083c7c37dc0a1ce4c51f
elgedr <elgedr@ttu.ee>
2020-10-13 14:28:21 +0300

    ff

--- EX/ex06_messenger/messenger.py
+++ EX/ex06_messenger/messenger.py
@@ -46,7 +46,7 @@ def get_messages_by_user(userrr: User, chattt: Chat) -> list:
     """Get messages by user in chat."""
     messages = []
     for messag in chattt.messages:
-        if userrr.name in messag.user.name:
+        if userrr in messag.user:
             messages.append(messag)
     return messages
 
