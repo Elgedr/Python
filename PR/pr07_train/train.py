7a37bfbfe3c82d15b0d5f2c3faa54275b97f08d9
elgedr <elgedr@ttu.ee>
2020-10-15 20:34:32 +0300

    r

--- PR/pr07_train/train.py
+++ PR/pr07_train/train.py
@@ -27,37 +27,30 @@ class Train:
 
     def get_seats_in_train(self) -> int:
         """Meetod, mis tagastab istmete koguarvu terve rongi peale."""
-        seats_count = self._carriages * self._seats_in_carriage
+        seats_count = self.carriages * self.seats_in_carriage
         return seats_count
 
     def get_number_of_passengers(self) -> int:
         """Tagastab rongi sisse tulevate reisijate arvu."""
-        return len(self._passengers)
+        return len(self.passengers)
 
     def get_passengers_in_carriages(self) -> dict:
         """Tagastab sõnastiku vagunite ja reisijate andmetega."""
         result = {}
-        car = int(self._carriages)
-        sit = int(self._seats_in_carriage)
-        for passenger in self.passengers:
-            if int(passenger._seat.split("-")[0]) > car or int(passenger._seat.split("-")[1]) > sit:
-                self.passengers.remove(passenger)
-        for i in range(1, self._carriages + 1):
-            result[str[i]] = []
-        for passen in self._passengers:
-            result[passen.carriage].append(passen.__dict__)
+        self.passengers = self.passengers
+        for i in range(1, self.carriages + 1):
+            result[str(i)] = []
+        for i in self.passengers:
+            result[i.seat.split("-")[0]].append(i.__dict__)
         return result
 
     @passengers.setter
     def passengers(self, value_list: list):
-        car = self._carriages
-        sit = self._seats_in_carriage
-        for passenger in value_list:
-            if int(passenger._seat.split("-")[0]) > car or int(passenger._seat.split("-")[1]) > sit:
-                value_list.remove(passenger)
-            else:
-                pass
-        self._passengers = value_list
+        set_list = []
+        for i in value_list:
+            if int(i.seat.split("-")[0]) <= self._carriages and int(i.seat.split("-")[1]) <= self._seats_in_carriage:
+                set_list.append(i)
+        self._passengers = set_list
 
     @carriages.setter
     def carriages(self, value: int):
@@ -73,12 +66,14 @@ class Passenger:
 
     def __init__(self, passenger_id: str, seat: str):
         """Constructor."""
-        self._passenger_id = passenger_id  # reisija unikaalne identifikaator (id)
-        self._seat = seat  # istekoha number.  vaguni_nr-istekoha_nr 2-14
+        self.passenger_id = passenger_id  # reisija unikaalne identifikaator (id)
+        self.seat = seat  # istekoha number.  vaguni_nr-istekoha_nr 2-14
 
     def __dict__(self):
         """Magic method."""
-        return {'id': self._passenger_id, 'seat': self._seat}
+        return {'id': self.passenger_id, 'seat': self.seat.split("-")[1]}
+
+
 
 
 if __name__ == '__main__':
