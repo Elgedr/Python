2638ee4d583d0fe66c8065af4dfdadb2c9eed0ae
elgedr <elgedr@ttu.ee>
2020-10-15 20:42:37 +0300

    r

--- PR/pr07_train/train.py
+++ PR/pr07_train/train.py
@@ -39,8 +39,8 @@ class Train:
         """Tagastab sõnastiku vagunite ja reisijate andmetega."""
         result = {}
         self.passengers = self.passengers
-        for i in range(1, self.carriages + 1):
-            result[str(i)] = []
+        for i in range(self.carriages):
+            result[str(i+1)] = []
         for i in self.passengers:
             result[i.seat.split("-")[0]].append(i.__dict__())
         return result
