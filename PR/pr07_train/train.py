e8a1bdbc945f8ccc4a4c08b702c67bda0adcdb2b
elgedr <elgedr@ttu.ee>
2020-10-16 19:01:58 +0300

    h

--- PR/pr07_train/train.py
+++ PR/pr07_train/train.py
@@ -32,16 +32,16 @@ class Train:
 
     def get_number_of_passengers(self) -> int:
         """Tagastab rongi sisse tulevate reisijate arvu."""
-        self.passengers = self.passengers
-        return len(self.passengers)
+        # self.passengers = self.passengers
+        return len(self._passengers)
 
     def get_passengers_in_carriages(self) -> dict:
         """Tagastab sõnastiku vagunite ja reisijate andmetega."""
         result = {}
-        self.passengers = self.passengers
+        # self.passengers = self.passengers  # первый self идет в
         for i in range(1, self.carriages + 1):
             result[str(i)] = []
-        for i in self.passengers:
+        for i in self._passengers:
             result[i.seat.split("-")[0]].append(i.__dict__())
         return result
 
@@ -73,3 +73,12 @@ class Passenger:
     def __dict__(self):
         """Magic method."""
         return {'id': self.passenger_id, 'seat': self.seat.split("-")[1]}
+
+
+if __name__ == '__main__':
+    p_1 = Passenger('123', '1-9')
+    p_2 = Passenger('321', '2-11')
+    p_3 = Passenger('456', '4-5')
+    t = Train([p_1, p_2, p_3], 3, 10)
+    print(t.passengers)
+    print(t.get_passengers_in_carriages())
