fed0be45976098c0690527a3a4f580a539f2ce95
elgedr <elgedr@ttu.ee>
2020-10-19 13:11:48 +0300

    w

--- EX/ex07_train_station/train_station.py
+++ EX/ex07_train_station/train_station.py
@@ -7,8 +7,6 @@ class Passenger:
         self._passenger_id = passenger_id  # reisija unikaalne identifikaator (id)
         self._seat = seat  # istekoha number (rongi_id-vaguni_nr-istekoha_nr)
 
-
-
     @property
     def id(self) -> str:
         """Decorator."""
@@ -26,11 +24,11 @@ class Train:
         self._train_id = train_id  # rongi unikaalne id
         self._carriages = carriages  # vagunite arv
         self._seats_in_carriage = seats_in_carriage  # ühes vagunis olevate istmete arv
-        self._passengers = []
+        self._passengers_filtred = []
 
     def __dict__(self):
         """Magic metod."""
-        seats = self.get_seats_in_train()
+        seats = f"{self.get_number_of_passengers()} / {self.get_seats_in_train()}"
         return {"train id": self._train_id, "carriages": self._carriages, "seats": seats}
 
     @property
@@ -48,7 +46,6 @@ class Train:
         """Decorator"""
         return self._seats_in_carriage
 
-
     def get_seats_in_train(self) -> int:
         """Meetod, mis tagastab istmete koguarvu terve rongi peale."""
         res = self._carriages * self._seats_in_carriage
@@ -56,25 +53,29 @@ class Train:
 
     def get_number_of_passengers(self) -> int:
         """Meetod, mis tagastab rongi reisijate arvu."""
-        #TODO CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCccccccccccccccccccccccccccccccccccccccccccccccc
-        res = len(self._passengers)
+        res = len(self._passengers_filtred)
+        return res
 
     def get_passengers_in_carriages(self) -> dict:
         """Meetod, mis tagastab sõnastiku vagunite ja reisijate andmetega."""
         res = {}
         for wagon_num in range(1, self._carriages + 1):
             res[str(wagon_num)] = []
-        for passanger in self._passengers:
+        for passanger in self._passengers_filtred:
             res[passanger.split("-")[1]].append(passanger)
         return res
 
     def add_passenger(self, passenger: Passenger):
         """Meetod, mis lisab reisija rongi peale rongijaamast."""
         p_id = passenger.seat.split('-')[0]
-        if p_id == self._train_id:
-            self._passengers.append(p_id)
-
-
+        p_seat = int(passenger.seat.split('-')[2])
+        p_wagon = int(passenger.seat.split('-')[1])
+        if p_id == self._train_id and p_wagon <= self._carriages and p_seat <= self._seats_in_carriage:
+            for passen in self._passengers_filtred:
+                if passenger.seat == passen.seat:
+                    return None
+            self._passengers_filtred.append(passenger)
+        return passenger
 
     @train_id.setter
     def train_id(self, value: str):
@@ -85,7 +86,6 @@ class Train:
     def seats_in_carriage(self, value: int):
         """Setter."""
         self._seats_in_carriage = value
-
 
 
 class TrainStation:
@@ -103,6 +103,20 @@ class TrainStation:
 
     def get_number_of_passengers(self):
         """Meetod, mis tagastab rongijaama (rongidesse paigutatud) reisijate koguarvu."""
+        return len(self._passengers)
+
+    def passangers_in_trains(self):
+        passangers_in_train = []
+        passangers_in_train.extend(self._passengers)
+        for pas in self._passengers:
+            pass_id = pas.seat.split('_')[0]
+            for tr in self._trains:
+                psng = None
+                if tr.train_id == pass_id:
+                    psng = tr.add_passenger(pas)
+                if not psng:
+                    passangers_in_train.remove(pas)
+        self._passengers = passangers_in_train
 
     @property
     def passengers(self):
