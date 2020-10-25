39bb7fe81cf6090ce6f57b8648ef28e07fb3b54e
elgedr <elgedr@ttu.ee>
2020-10-26 01:04:43 +0200

    q

--- EX/ex08_formula_one/formula_one.py
+++ EX/ex08_formula_one/formula_one.py
@@ -202,7 +202,7 @@ class FormulaOne:
             file.write(f'PLACE{5 * " "}NAME{21 * " "}TEAM{21 * " "}TIME{11 * " "}DIFF{11 * " "}POINTS{0 * " "}\n'
                        f'{96 * "-"}\n')
             for dictionaries in filtered_file:
-                file.write(f'{dictionaries["Place"]}{(10 - len(str(dictionaries["Place"]))) * " "}'
+                file.write(f'{dictionaries["Place"]}{(10 - len(str(dictionaries["Place"]))) * " "}'  # write записывает 1 строку в файл
                            f'{dictionaries["Name"]}{(25 - len(dictionaries["Name"])) * " "}'
                            f'{dictionaries["Team"]}{(25 - len(str(dictionaries["Team"]))) * " "}'
                            f'{dictionaries["Time"]}{(15 - len(str(dictionaries["Time"]))) * " "}'
@@ -218,7 +218,12 @@ class FormulaOne:
 
         :param race_number: Race to write to file
         """
-        pass
+        file_name = f'race_{race_number}_results.csv'
+        with open(file_name, mode="w", newline="") as csvfile:
+            writer = csv.writer(csvfile, delimiter=',')
+            writer.writerow(['Place', 'Name', 'Team', ' Time', ' Diff', ' Points', 'Race'])
+            for dictionary in self._race.get_results_by_race(race_number):
+                writer.writerow([dictionary["Place"], dictionary["Name"], dictionary["Team"], dictionary["Diff"], dictionary["Points"], dictionary["Race"]])
 
     def write_championship_to_file(self):
         """
