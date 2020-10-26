e7c2bc8a0cefe8a1747ddf869b93415851d0bcda
elgedr <elgedr@ttu.ee>
2020-10-26 11:27:49 +0200

    q

--- EX/ex08_formula_one/formula_one.py
+++ EX/ex08_formula_one/formula_one.py
@@ -221,9 +221,9 @@ class FormulaOne:
         file_name = f'race_{race_number}_results.csv'
         with open(file_name, mode="w", newline="") as csvfile:
             writer = csv.writer(csvfile, delimiter=',')
-            writer.writerow(['Place', 'Name', 'Team', 'Time', 'Diff', 'Points'])
+            writer.writerow(['Place', 'Name', 'Team', 'Time', 'Diff', 'Points', 'Race'])
             for dictionary in self._race.get_results_by_race(race_number):
-                writer.writerow([dictionary["Place"], dictionary["Name"], dictionary["Team"], dictionary["Diff"], dictionary["Points"]])
+                writer.writerow([dictionary["Place"], dictionary["Name"], dictionary["Team"], dictionary["Diff"], dictionary["Points"], race_number])
 
     def write_championship_to_file(self):
         """
