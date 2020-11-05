f615b3a7e6b713e0d288ea7aad60ec0803b06a50
elgedr <elgedr@ttu.ee>
2020-11-05 19:23:44 +0200

    dd

--- KT/kt4/exam.py
+++ KT/kt4/exam.py
@@ -97,8 +97,12 @@ def create_student(name: str, grades: list, credit_points: int) -> Student:
     Round the average grade up to three decimal places.
     If the list of grades is empty, the average grade will be 0.
     """
-    average = round(sum(grades) / len(grades), 3)
-    student1 = Student(name, average, credit_points)
+    if not list:
+        average = 0
+        return Student(name, average, credit_points)
+    else:
+        average = round(sum(grades) / len(grades), 3)
+        student1 = Student(name, average, credit_points)
     return student1
 
 
