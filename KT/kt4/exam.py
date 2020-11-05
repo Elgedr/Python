a15638e5813dd910e2a61c8b047b93514b91b49e
elgedr <elgedr@ttu.ee>
2020-11-05 19:44:27 +0200

    dd

--- KT/kt4/exam.py
+++ KT/kt4/exam.py
@@ -117,7 +117,7 @@ def get_top_student_with_credit_points(students: list, min_credit_points: int):
     for student in students:
         if student.credit_points >= min_credit_points:
             studentss.append(student)
-        res = sorted(studentss, key=lambda x: x.average_grade)
+        res = sorted(studentss, key=lambda x: -x.average_grade)
         if not res:
             return None
         return res[0]
