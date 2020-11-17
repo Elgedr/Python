b614a1bfaa4537b28431006d386d909a03f3c098
elgedr <elgedr@ttu.ee>
2020-11-18 01:24:16 +0200

    q

--- PR/pr12_zoo/zoo.py
+++ PR/pr12_zoo/zoo.py
@@ -1,6 +1,6 @@
 """A small exercise in zookeeping."""
-import math
 from functools import reduce
+import math
 
 
 class Animal:
@@ -60,7 +60,11 @@ def find_how_many_pumpkins_are_needed_to_feed_animals(animal_list: list) -> int:
     :param animal_list: input list
     :return: amount of pumpkins needed to sustain all the animals over the winter (rounded up).
     """
-    pass
+    b = list(filter(lambda x: x.diet != "carnivorous", animal_list))
+    print(b)
+    a = reduce(lambda x, y: (x + ((y.weight_range[0] + y.weight_range[1]) / 2)), b, 0) * 0.06 / 3 * 180
+    res = math.ceil(a)
+    return res
 
 
 def sort_alphabetically_by_scientific_name(animal_list: list) -> list:
