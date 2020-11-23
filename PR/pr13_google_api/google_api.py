8d428bdb387c5cfacaea3ec480e7f4bc7882b5e6
elgedr <elgedr@ttu.ee>
2020-11-23 22:18:02 +0200

    s

---
+++ PR/pr13_google_api/google_api.py
@@ -0,0 +1,7 @@
+def get_links_from_spreadsheet(id: str, token: str) -> list:
+    """Should get a list of strings from the first column of a Google Spreadsheet with the given ID."""
+    return []
+
+def get_links_from_playlist(link: str, developer_key: str) -> list:
+    """Should get a list of links to songs in the Youtube playlist with the given address."""
+    return []
