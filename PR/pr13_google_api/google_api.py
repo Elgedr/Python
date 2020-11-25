039049f36d23a64c43ad74d29dc5b4686d9cd9c5
elgedr <elgedr@ttu.ee>
2020-11-25 22:43:29 +0200

    x

--- PR/pr13_google_api/google_api.py
+++ PR/pr13_google_api/google_api.py
@@ -64,7 +64,7 @@ def get_links_from_playlist(link: str, developer_key: str) -> list:
     developer_keyy = developer_key
     id = link.split('=')[1]
 
-    # Get credentials and create an API clientt
+    # Get credentials and create an API client
     youtube = googleapiclient.discovery.build(
         api_service_name, api_version, developerKey=developer_keyy, maxResults=50)
 
