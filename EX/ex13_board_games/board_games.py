d226e8d21bf3021da1a0121dbc30bc0ac3887e4b
elgedr <elgedr@ttu.ee>
2020-11-30 15:55:27 +0200

    x

--- EX/ex13_board_games/board_games.py
+++ EX/ex13_board_games/board_games.py
@@ -1,6 +1,4 @@
 """Board games."""
-import os
-import os.path
 
 
 class Statistics:
@@ -8,11 +6,29 @@ class Statistics:
 
     def __init__(self, filename: str):
         """Statistics constructor."""
+        self.games = {}
+        self.players = {}
         self.filename = filename
 
     def __get__(self, path: str):
         """Get a path."""
-        pass
+        tokens = path[1::].split("/")  # our path = /game/{name}/amount we will get ["game", "{name}", "amount"]
+        func = getattr(self,  "get_" + tokens[0])  # get_game
+        return func(tokens)
+
+    def make_dict_of_players(self, filename: str):
+        with open(filename, encoding='utf-8') as f:
+            res = f.read().splitlines()
+        return res
+
+
+    def get_games(self, x):
+        game_name = x[0]
+        game = self.games
+
+    def get_players(self, x):
+        player_name = x[0]
+
 
 
 class Player:
@@ -25,9 +41,10 @@ class Player:
 class Game:
     """Game class."""
 
-    def __init__(self):
+    def __init__(self, file: str):
         """Game constructor."""
 
 
 if __name__ == '__main__':
     statistics = Statistics("ex13_test_file.txt")
+    print()
