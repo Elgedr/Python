29c1c4bd4260bfa9b7749d91318830792358a2f1
elgedr <elgedr@ttu.ee>
2020-12-01 19:32:44 +0200

    з

--- PR/pr14_robot_api/robot.py
+++ PR/pr14_robot_api/robot.py
@@ -1,4 +1,5 @@
 from PiBot import PiBot  # robot class
+"""Robot class."""
 robot = PiBot()  # robot object
 
 a = robot.get_left_line_sensors()
@@ -7,5 +8,6 @@ robot.set_wheels_speed(30)
 while a[0] > 0:
     robot.set_wheels_speed(30)
     a = robot.get_left_line_sensors()
+    print(a)
     robot.sleep(0.05)
 robot.set_wheels_speed(0)
