18c94c91236758bebd3010978f0b7c90e1f9f794
elgedr <elgedr@ttu.ee>
2020-12-01 18:31:13 +0200

    v

--- PR/pr14_robot_api/robot.py
+++ PR/pr14_robot_api/robot.py
@@ -12,21 +12,21 @@ while distance_from_object > 0.01:
 # Stop the robot when done
 robot.set_wheels_speed(0)
 
-print(robot.set_wheels_speed(30))  # percentage of robots max speed
-print(robot.sleep(2))
-print(robot.set_wheels_speed(0))
-print(robot.get_front_lasers())
-
-print(robot.get_rear_irs())
-print(robot.get_distance_sensors())
-
-print(robot.get_leftmost_line_sensor())
-print(robot.get_second_line_sensor_from_left())
-print(robot.get_third_line_sensor_from_left())
-
-print(robot.get_rightmost_line_sensor())
-print(robot.get_second_line_sensor_from_right())
-print(robot.get_third_line_sensor_from_right())
-
-print(robot.get_rotation())  # на сколько градусов он повернулся
-print(robot.WHEEL_DIAMETER)
+# print(robot.set_wheels_speed(30))  # percentage of robots max speed
+# print(robot.sleep(2))
+# print(robot.set_wheels_speed(0))
+# print(robot.get_front_lasers())
+#
+# print(robot.get_rear_irs())
+# print(robot.get_distance_sensors())
+#
+# print(robot.get_leftmost_line_sensor())
+# print(robot.get_second_line_sensor_from_left())
+# print(robot.get_third_line_sensor_from_left())
+#
+# print(robot.get_rightmost_line_sensor())
+# print(robot.get_second_line_sensor_from_right())
+# print(robot.get_third_line_sensor_from_right())
+#
+# print(robot.get_rotation())  # на сколько градусов он повернулся
+# print(robot.WHEEL_DIAMETER)
