5f0fa1a0dc5be5d6d2aa0d7f198c3e1f4b4fe8b5
elgedr <elgedr@ttu.ee>
2020-12-01 19:18:50 +0200

    з

--- PR/pr14_robot_api/robot.py
+++ PR/pr14_robot_api/robot.py
@@ -60,4 +60,7 @@ robot.set_wheels_speed(30)
 print(robot.sleep(2))
 print(robot.set_wheels_speed(0))
 print(robot.get_left_line_sensors())
-
+robot.set_wheels_speed(30)
+print(robot.sleep(2))
+print(robot.set_wheels_speed(0))
+print(robot.get_left_line_sensors())
