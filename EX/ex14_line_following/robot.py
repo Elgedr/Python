2d245bd30d5801763a1606b61a888f2eda09364b
elgedr <elgedr@ttu.ee>
2020-12-07 21:27:54 +0200

    з

--- EX/ex14_line_following/robot.py
+++ EX/ex14_line_following/robot.py
@@ -29,21 +29,29 @@ robot = PiBot()  # robot object
 #     a = robot.get_left_line_sensors()
 #     print(a)
 #     robot.sleep(0.01)
-for i in range(100):
-    a = robot.get_left_line_sensors()
-    b = robot.get_right_line_sensors()
-    while a[0] < 1024 and b[0] < 1024:
-        robot.set_wheels_speed(50)
-        robot.sleep(0.01)
-    else:
-         robot.set_left_wheel_speed(2)
-         robot.set_right_wheel_speed(0)
-         robot.sleep(0.2)
+# for i in range(100):
+#     a = robot.get_left_line_sensors()
+#     b = robot.get_right_line_sensors()
+#     while a[0] < 1024 and b[0] < 1024:
+#         robot.set_wheels_speed(50)
+#         robot.sleep(0.01)
+#     else:
+#          robot.set_left_wheel_speed(2)
+#          robot.set_right_wheel_speed(0)
+#          robot.sleep(0.2)
 
 # print(robot.get_rotation())
 # for i in range(50):
 #     robot.set_wheels_speed(50)
 #     robot.sleep(0.01)
+a = robot.get_left_line_sensors()
+b = robot.get_right_line_sensors()
+if a[0] < 1024 and b[0] < 1024:
+        robot.set_wheels_speed(50)
+        robot.sleep(0.01)
+while a[0] < 1024 and b[0] < 1024:
+    robot.set_wheels_speed(50)
+    robot.sleep(0.01)
 
 # robot.set_wheels_speed(30)
 # robot.sleep(0.05)
