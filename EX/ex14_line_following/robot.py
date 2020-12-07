8143d9f8c7b7e6c6940b54b9a00af701cf639b15
elgedr <elgedr@ttu.ee>
2020-12-07 20:26:37 +0200

    з

--- EX/ex14_line_following/robot.py
+++ EX/ex14_line_following/robot.py
@@ -3,17 +3,16 @@ from PiBot import PiBot  # robot class
 robot = PiBot()  # robot object
 
 a = robot.get_left_line_sensors()
+b = robot.get_leftmost_line_sensor()
 
 robot.set_wheels_speed(30)
-while a[0] > 0:
-    robot.set_left_wheel_speed(-2)
-    robot.set_wheels_speed(2)
-    a = robot.get_left_line_sensors()
-    print(a)
-    robot.sleep(0.01)
-for i in range(50):
-    robot.set_wheels_speed(50)
-    robot.sleep(0.01)
+for u in range(500):
+    while a[0] > 0 and b[0] > 0:
+        robot.set_left_wheel_speed(-2)
+        robot.set_wheels_speed(2)
+        a = robot.get_left_line_sensors()
+        print(a)
+        robot.sleep(0.01)
 
 
 robot.set_wheels_speed(30)
