841231f43d5adca312bffce467b123af6f4700c9
elgedr <elgedr@ttu.ee>
2020-12-07 21:39:43 +0200

    з

--- EX/ex14_line_following/robot.py
+++ EX/ex14_line_following/robot.py
@@ -2,48 +2,6 @@
 from PiBot import PiBot  # robot class
 robot = PiBot()  # robot object
 
-# a = robot.get_left_line_sensors()
-# b = robot.get_right_line_sensors()
-#
-# robot.set_wheels_speed(30)
-# for u in range(500):
-#     while a[0] > 0 and b[0] > 0:
-#         robot.set_wheels_speed(50)
-#         a = robot.get_left_line_sensors()
-#         print(a)
-#         robot.sleep(0.01)
-#     else:
-#         robot.set_left_wheel_speed(-20)
-#         robot.set_right_wheel_speed(20)
-#
-#
-#
-# robot.set_wheels_speed(30)
-# robot.sleep(0.05)
-# robot.set_wheels_speed(0)
-# a = robot.get_left_line_sensors()
-#
-# robot.set_wheels_speed(30)
-# while a[0] > 0:
-#     robot.set_wheels_speed(50)
-#     a = robot.get_left_line_sensors()
-#     print(a)
-#     robot.sleep(0.01)
-# for i in range(100):
-#     a = robot.get_left_line_sensors()
-#     b = robot.get_right_line_sensors()
-#     while a[0] < 1024 and b[0] < 1024:
-#         robot.set_wheels_speed(50)
-#         robot.sleep(0.01)
-#     else:
-#          robot.set_left_wheel_speed(2)
-#          robot.set_right_wheel_speed(0)
-#          robot.sleep(0.2)
-
-# print(robot.get_rotation())
-# for i in range(50):
-#     robot.set_wheels_speed(50)
-#     robot.sleep(0.01)
 for i in range(2100):
     a = robot.get_left_line_sensors()
     b = robot.get_right_line_sensors()
@@ -56,7 +14,5 @@ for i in range(2100):
         robot.set_right_wheel_speed(0)
         robot.sleep(0.01)
 
-# robot.set_wheels_speed(30)
-# robot.sleep(0.05)
 robot.set_wheels_speed(0)
 robot.done()
