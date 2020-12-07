dfe0f434007536838590554d04fe32aa73608af1
elgedr <elgedr@ttu.ee>
2020-12-07 21:36:48 +0200

    з

--- EX/ex14_line_following/robot.py
+++ EX/ex14_line_following/robot.py
@@ -44,7 +44,7 @@ robot = PiBot()  # robot object
 # for i in range(50):
 #     robot.set_wheels_speed(50)
 #     robot.sleep(0.01)
-for i in range(100):
+for i in range(500):
     a = robot.get_left_line_sensors()
     b = robot.get_right_line_sensors()
 
