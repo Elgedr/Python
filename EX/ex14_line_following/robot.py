f5a1105afc1e485443066e4885b89a06bc4c03f7
elgedr <elgedr@ttu.ee>
2020-12-07 20:32:18 +0200

    з

--- EX/ex14_line_following/robot.py
+++ EX/ex14_line_following/robot.py
@@ -8,11 +8,14 @@ b = robot.get_right_line_sensors()
 robot.set_wheels_speed(30)
 for u in range(500):
     while a[0] > 0 and b[0] > 0:
-        robot.set_left_wheel_speed(-2)
-        robot.set_wheels_speed(2)
+        robot.set_wheels_speed(50)
         a = robot.get_left_line_sensors()
         print(a)
         robot.sleep(0.01)
+    else:
+        robot.set_left_wheel_speed(-20)
+        robot.set_right_wheel_speed(20)
+
 
 
 robot.set_wheels_speed(30)
